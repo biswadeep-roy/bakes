342db6e3bb6f144cc8caf6809201c59a1adf2ef0
Biswadeep Roy <biswadeeproy1230@gmail.com>
2023-12-31 22:39:21 +0530

    just a commit

--- register.js
+++ register.js
@@ -86,3 +86,91 @@ const register = () => {
 }
 
 export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
+import React from 'react'
+
+const register = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default register
